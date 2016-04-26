77f203a49794950d96fdf77f08bae6347371857c
Ben Lebson <blebson@gmail.com>
2016-04-25 21:40:33 -0700

    Updated Version

--- devicetypes/blebson/dcs-930l.src/dcs-930l.groovy
+++ devicetypes/blebson/dcs-930l.src/dcs-930l.groovy
@@ -1,5 +1,5 @@
 /**
- *	D-Link DCS-930L v1.0.0
+ *	D-Link DCS-930L v1.0.1
  *  Image Capture and Video Streaming courtesy Patrick Stuart (patrick@patrickstuart.com)
  *  
  *  Copyright 2015 blebson
